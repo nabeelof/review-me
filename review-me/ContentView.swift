64209dc9c5c03e5a6e49c1db38c6b79dd9372432
nabeel <nabeel.iriq.g@gmail.com>
2025-01-06 23:45:00 +0300

    Initial Commit

---
+++ review-me/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  review-me
+//
+//  Created by nabil on 06/01/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ review-me/review_meApp.swift
@@ -0,0 +1,17 @@
+//
+//  review_meApp.swift
+//  review-me
+//
+//  Created by nabil on 06/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct review_meApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
