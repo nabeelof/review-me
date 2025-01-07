454cac13c07e0e063d2a9ceff23ee68c9bdee327
nabeel <nabeel.iriq.g@gmail.com>
2025-01-07 04:41:41 +0300

    Add out-of-range handling for invalid employeeIndex set by tester

--- review-me/View/Main/MainView.swift
+++ review-me/View/Main/MainView.swift
@@ -7,9 +7,11 @@
 
 import SwiftUI
 
+// Note: For testing purposes, you can modify employeeIndex to explore different reviewer options.
+// In this branch, I followed the home task instructions. However, for improved user experience and easier testing, refer to the "enhanced" branch.
 struct MainView: View {
     @StateObject private var viewModel = MainViewModel()
-    let employeeIndex = 1 // for testing: change this to try other reviewers
+    let employeeIndex = 1
     @State var showResult = false
     
     var body: some View {
@@ -19,22 +21,28 @@ struct MainView: View {
                     ProgressView("Loading ...")
                         .progressViewStyle(CircularProgressViewStyle())
                 } else {
-                    VStack {
-                        Text("Hello: \(viewModel.employees[employeeIndex].name ?? "Unknown")")
-                            .font(.title).bold()
-                        if showResult {
-                            ResultView(employee: viewModel.employees[employeeIndex])
-                        } else {
-                            Button(action: {
-                                showResult = true
-                            }) {
-                                Text("Reveal Names")
-                                    .font(.headline)
-                                    .padding()
-                                    .background(Color.black)
-                                    .foregroundColor(.white)
-                                    .cornerRadius(10)
-                            }.padding()
+                    if(employeeIndex >= viewModel.employees.count){
+                        Text("Invalid employeeIndex")
+                            .font(.headline)
+                            .foregroundColor(.black)
+                    } else {
+                        VStack {
+                            Text("Hello: \(viewModel.employees[employeeIndex].name ?? "Unknown")")
+                                .font(.title).bold()
+                            if showResult {
+                                ResultView(employee: viewModel.employees[employeeIndex])
+                            } else {
+                                Button(action: {
+                                    showResult = true
+                                }) {
+                                    Text("Reveal Names")
+                                        .font(.headline)
+                                        .padding()
+                                        .background(Color.black)
+                                        .foregroundColor(.white)
+                                        .cornerRadius(10)
+                                }.padding()
+                            }
                         }
                     }
                 }
